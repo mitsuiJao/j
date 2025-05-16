43eb00291212f998d8a291ac1e74bccb4a716f24
mitsuijao <nishima.kun@gmail.com>
2025-05-17 00:37:42 +0900

    a;lskfja;lej

--- 4/algorithm/week4/algo-data-4-1/algo-data-4-1.c
+++ 4/algorithm/week4/algo-data-4-1/algo-data-4-1.c
@@ -4,9 +4,17 @@
 int main(void){
     int h;
     scanf("%d", &h);
-    int tree[512];
-    for (int i=0; i<(int)pow(2.0, (h-1)); i++){
+    int tree[1023];
+    for (int i=0; i<(int)pow(2.0, h)-1; i++){
         scanf("%d", &tree[i]);
+    }
+    int count = 0;
+    for (int i=0; i<h; i++){
+        printf("%d ", i+1);
+        for (int j=0; j<(int)pow(2.0, i); j++){
+            printf("%d ", tree[count++]);
+        }
+        printf("\n");
     }
     return 0;
 }
