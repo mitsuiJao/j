9cc03c7c3bcba0d7fa78134bb5c417928e1bc1a0
mitsuijao <nishima.kun@gmail.com>
2025-05-19 00:12:29 +0900

    algo 4-2

--- 4/algorithm/week4/algo-data-4-2/algo-data-4-2.c
+++ 4/algorithm/week4/algo-data-4-2/algo-data-4-2.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
 #include <math.h>
 
+int count = 0;
+
 int recursive(int *arr, int size) {
     if (size == 1){
         return arr[0];
     }
     int asize =(int)floor(size/2.0);
     int a[asize], b[size-asize];
-    for (int i=0; i<=floor((size-1)/2); i++){
-
+    for (int i=0; i<asize; i++){
+       a[i] = arr[i];
     }
+    for (int i=0; i<size-asize; i++){
+       b[i] = arr[i+asize];
+    }
+
+    int x = recursive(a, asize);
+    int y = recursive(b, size-asize);
+    
+    count++;
+    return x+y;
 }
 
 int main(void){
@@ -19,6 +30,8 @@ int main(void){
     for (int i=0; i<n; i++){
         scanf("%d", &arr[i]);
     }
+    printf("%d\n", recursive(arr, n));
+    printf("%d\n", count);
 
-    
+    return 0;
 }
---
+++ 4/algorithm/week4/algo-data-4-2/test.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main(void){
+    printf("%d", 5/2);
+    return 0;
+}
