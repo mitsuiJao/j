7dc8b2c1b70707c1b0e8174f220ec97dc5a85820
mitsuijao <nishima.kun@gmail.com>
2025-07-10 12:44:56 +0900

    week10-2 tochuU

---
+++ 4/algorithm/week10/algo-data-10-2/algo-data-10-2.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+void swap(int a[], int from, int to){
+    int tmp = a[to];
+    a[to] = a[from];
+    a[from] = tmp;
+}
+
+void selection (int a[], int n, int *b){
+    *b = 0;
+    for (int i=0; i<n-1; i++){
+        int min = 1;
+        for (int j=i+1; j<n; j++){
+            (*b)++;
+            if (a[j] < a[min]){
+                min = j;
+            }
+        }
+        swap(a, i, min);
+    }
+}
+
+int main (void){
+    int n, b;
+    scanf("%d", &n);
+    int *x = calloc(n, sizeof(int));
+
+    for (int i=0; i<n; i++){
+        scanf("%d", &x[i]);
+    }
+
+    selection(x, n, &b);
+
+    for (int i=0; i<n; i++){
+        printf("%d ", x[i]);
+    }
+    printf("\n%d\n", b);
+    free(x);
+    return 0;
+}
